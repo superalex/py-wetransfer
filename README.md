d990d902bf2eab045870418897457d0f1e8c61dc
Alejandro Alonso <alejandro.alonso@kaleidos.net>
2013-03-23 13:26:39 +0100

    Download directly from the link

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ py-wetransfer
 =============
 
 Python script for downloading wetransfer files (https://www.wetransfer.com/) in command line mode
+
+Usage
+=====
+
+You should have a we transfer address similar to https://www.wetransfer.com/downloads/XXXXXXXXXX/YYYYYYYYY/ZZZZZZZZ
+
+So execute:
+    python wetransfer.py -u https://www.wetransfer.com/downloads/XXXXXXXXXXXXXXXXXXXXXXXXX/YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY/ZZZZZ
+
+And download it! :)
+
+Requirements
+=============
+python
+requests
+
+.. code-block:: console
+
+    pip install requests
+
+
--- wetransfer.py
+++ wetransfer.py
@@ -1,12 +1,11 @@
 from urlparse import urlparse, parse_qs
-import requests, sys, json, pprint, re, getopt
+import requests, sys, json, re, getopt
 
 def download(file_id, recipient_id, security_hash):
     url = "https://www.wetransfer.com/api/v1/transfers/{0}/download?recipient_id={1}&security_hash={2}&password=&ie=false".format(file_id, recipient_id, security_hash)
 
     r = requests.get(url)
     download_data = json.loads(r.content)
-    pprint.pprint(download_data)
     print "Downloading..."
     if download_data.has_key('direct_link'):
         content_info_string = parse_qs(urlparse(download_data['direct_link']).query)['response-content-disposition'][0]
@@ -19,30 +18,33 @@ def download(file_id, recipient_id, security_hash):
     output_file = open(file_name, 'w')
     output_file.write(r.content)
     output_file.close()
-    print "Finished!"
+    print "Finished! {0}".format(file_name)
 
 def usage():
     print """
-https://www.wetransfer.com/downloads/XXXXXXXXXX/YYYYYYYYY/ZZZZZZZZ
-python wetransfer.py -f XXXXXXXXXX -r YYYYYYYYY -s ZZZZZZZZ
+You should have a we transfer address similar to https://www.wetransfer.com/downloads/XXXXXXXXXX/YYYYYYYYY/ZZZZZZZZ
+
+So execute:
+    python wetransfer.py -u https://www.wetransfer.com/downloads/XXXXXXXXXXXXXXXXXXXXXXXXX/YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY/ZZZZZ
+
+And download it! :)
 """
     sys.exit()
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "f:r:s:", ['file_id=', 'recipient_id', 'security_hash=',])
-        file_id = recipient_id = security_hash = None
+        opts, args = getopt.getopt(argv, "u:h", ['url', 'help'])
+        url = None
         for opt, arg in opts:
-            if opt in ('-f', '--file'):
-                file_id = arg
-            elif opt in ('-r', '--recipient'):
-                recipient_id = arg
-            elif opt in ('-s', '--security'):
-                security_hash = arg
+            if opt in ('-u', '--url'):
+                url = arg
+            if opt in ('-h', '--help'):
+                usage()
 
-        if len(opts) < 3:
+        if not url:
             usage()
 
+        [file_id, recipient_id, security_hash] = url.split('/')[-3:]
         download(file_id, recipient_id, security_hash)
 
     except getopt.GetoptError:
